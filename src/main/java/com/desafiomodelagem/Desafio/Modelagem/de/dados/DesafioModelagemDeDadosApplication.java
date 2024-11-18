2348b7b3634d3177dde8eb61d386eaac43942df7
Marcus <marcuspetit@hotmail.com>
2024-11-18 19:28:12 -0300

    iniciando

---
+++ src/main/java/com/desafiomodelagem/Desafio/Modelagem/de/dados/DesafioModelagemDeDadosApplication.java
@@ -0,0 +1,13 @@
+package com.desafiomodelagem.Desafio.Modelagem.de.dados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioModelagemDeDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioModelagemDeDadosApplication.class, args);
+	}
+
+}
